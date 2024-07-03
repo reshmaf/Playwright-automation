ea8535785f88964856895f6d8102299eb60da9e2
reshmaf <reshmafredy999@gmail.com>
2024-07-03 22:27:05 +0530

    Added new tests

--- swag-labs/products.spec.ts
+++ swag-labs/products.spec.ts
@@ -17,20 +17,33 @@ test('has hamburger icon', async({page}) =>{
     
 });
 
-test('hamberger menu options', async({page}) => {
+test('hamburger menu options', async({page}) => {
 
   const expectedOptions = ['All Items', 'About', 'Logout', 'Reset App State'];
 
   await page.locator('.bm-burger-button').click();
 
   // const menuOptions = page.locator('.menu-item');
-  // await expect(menuOptions).toHaveCount(4);
+  // await expect(menuOptions).toHaveCount(4); //check count
 
   const menuOptions = page.$$('.menu-item');
+  expect((await menuOptions).length).toBe(4);
 
   for(const menu of await menuOptions){
-    console.log(await menu.innerText());
-    //expect(expectedOptions).toContain(await menu.innerText());
-  }
-  
+    //console.log(await menu.innerText()); //print options
+    expect(expectedOptions).toContain(await menu.innerText());
+  } 
+
+});
+
+test('close hamburger menu', async({page}) =>{
+  const hamburgerIcon = page.locator('.bm-burger-button');
+  await hamburgerIcon.click();
+
+  const closeIcon = page.locator('.bm-cross-button');
+  await expect(closeIcon).toBeVisible();
+  await closeIcon.click();
+
+  await expect(hamburgerIcon).toBeVisible();
+
 });
