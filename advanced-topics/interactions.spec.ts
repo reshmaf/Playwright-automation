f733a52e162bf17b15d88a94e9c0c0c38ea55844
reshmaf <reshmafredy999@gmail.com>
2024-07-10 22:49:54 +0530

    Added test for drag and drop

---
+++ advanced-topics/interactions.spec.ts
@@ -0,0 +1,25 @@
+import { expect, Expect, test } from "@playwright/test";
+
+test.beforeEach('open website', async ({page}) =>{
+
+    await page.goto('https://demoqa.com/');
+
+    const interactionsIcon = page.locator('div.card-body > h5', { hasText: 'Interactions' });
+    const parentIcon = interactionsIcon.locator('..');
+
+    await parentIcon.click();
+    expect((await page.waitForSelector('xpath=//*[@id="app"]/div/div/div/div[1]/div/div/div[5]/span/div/div[1]')).isVisible());
+
+    const droppableIcon = page.locator('.text', { hasText: 'Droppable' });
+    await droppableIcon.click();
+});
+
+test('drag and drop', async ({page}) =>{
+    const dragObject = page.locator('#draggable');
+    const dropObject = page.locator('xpath=/html/body/div[2]/div/div/div/div[2]/div[2]/div/div[1]/div/div[2]');
+
+    await dragObject.dragTo(dropObject);
+
+    expect(page.locator('div#droppable > p', { hasText: 'Dropped!' }).isVisible());
+
+});
